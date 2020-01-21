3e8c79a6eaedf23d299afed7e3563c27beae1949
YoungChulShin <go1323@gmail.com>
2020-01-22 08:50:55 +0900

    First try - wrong

--- 647-PalindromicSubstrings/csharp/Program.cs
+++ 647-PalindromicSubstrings/csharp/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace csharp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solution solution = new Solution();
+            int count = solution.CountSubstrings("abc");
+            int count2 = solution.CountSubstrings("aaa");
+        }
+    }
+
+    public class Solution
+    {
+        // My First Method
+        public int CountSubstrings(string s) 
+        {
+            List<string> result = new List<string>();
+            List<string> palindromicList = new List<string>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result.Add(s[i].ToString());
+
+                if (palindromicList.Count > 0)
+                {
+                    if (palindromicList[0].StartsWith(s[i]))
+                    {
+                        for (int j = 0; j < palindromicList.Count; j++)
+                        {
+                            palindromicList[j] += s[i];
+                            result.Add(palindromicList[j]);
+                        }
+                    }
+                    else
+                    {
+                        palindromicList.Clear();
+                    }
+                }
+
+                palindromicList.Add(s[i].ToString());
+            }
+
+            return result.Count;
         }
     }
 }
